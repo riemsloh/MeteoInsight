51f584af4e45827704f237d28c76873f788bb943
riemsloh <olaf.lueg@gmail.com>
2025-06-08 17:08:42 +0200

    Initial Commit

---
+++ MeteoInsight/MeteoInsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeteoInsightApp.swift
+//  MeteoInsight
+//
+//  Created by Olaf on 08.06.25.
+//
+
+import SwiftUI
+
+@main
+struct MeteoInsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
